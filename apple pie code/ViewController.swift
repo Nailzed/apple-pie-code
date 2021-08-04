5bd012212874f7917cd33a4275be963a6391e279
Сергей Земсков <nailzed@yandex.ru>
2021-08-04 12:33:32 +0300

    added non UI code

--- apple pie code/ViewController.swift
+++ apple pie code/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: Properties
+    //MARK: - UI Properties
     let buttonStackView = UIStackView()
     let correctWordLabel = UILabel()
     var letterButtons = [UIButton]()
@@ -17,9 +17,183 @@ class ViewController: UIViewController {
     let topStackView = UIStackView()
     let treeImageView = UIImageView()
     
-    //MARK: Methods
-    @objc func  buttonPressed(_ sender: UIButton) {
-        print(#line, #function, sender.title(for: .normal) ?? "nil")
+    //MARK: - Properties
+    var currentGame: Game!
+        let incorrectMovesAllowed = 7
+        var listOfWords = [
+            "Австралия",
+            "Австрия",
+            "Азербайджан",
+            "Албания",
+            "Англия",
+            "Андорра",
+            "Аргентина",
+            "Армения",
+            "Афганистан",
+            "Белоруссия",
+            "Бельгия",
+            "Болгария",
+            "Боливия",
+            "Босния и Герцеговина",
+            "Бразилия",
+            "Бруней",
+            "Буркина - Фасо",
+            "Великобритания",
+            "Венгрия",
+            "Венесуэла",
+            "Вьетнам",
+            "Гаити",
+            "Гана",
+            "Гватемала",
+            "Гвинея",
+            "Германия",
+            "Гибралтар",
+            "Гондурас",
+            "Гонконг",
+            "Греция",
+            "Грузия",
+            "Дания",
+            "Египет",
+            "Зимбабве",
+            "Израиль",
+            "Индия",
+            "Индонезия",
+            "Иордания",
+            "Ирак",
+            "Иран",
+            "Ирландия",
+            "Исландия",
+            "Испания",
+            "Италия",
+            "Йемен",
+            "Кабо - Верде",
+            "Казахстан",
+            "Камбоджа",
+            "Камерун",
+            "Канада",
+            "Катар",
+            "Кения",
+            "Киргизия",
+            "Китай",
+            "Колумбия",
+            "Корея",
+            "Коста - Рика",
+            "Куба",
+            "Кувейт",
+            "Лаос",
+            "Латвия",
+            "Либерия",
+            "Ливан",
+            "Ливия",
+            "Литва",
+            "Лихтенштейн",
+            "Люксембург",
+            "Маврикий",
+            "Мавритания",
+            "Мадагаскар",
+            "Малайзия",
+            "Мали",
+            "Мальдивы",
+            "Мальта",
+            "Марокко",
+            "Мексика",
+            "Мозамбик",
+            "Молдавия",
+            "Монако",
+            "Монголия",
+            "Намибия",
+            "Непал",
+            "Нигерия",
+            "Нидерланды",
+            "Никарагуа",
+            "Новая Зеландия",
+            "Норвегия",
+            "ОАЭ",
+            "Оман",
+            "Пакистан",
+            "Панама",
+            "Папуа — Новая Гвинея",
+            "Парагвай",
+            "Перу",
+            "Польша",
+            "Португалия",
+            "Пуэрто - Рико",
+            "Кипр",
+            "Россия",
+            "Руанда",
+            "Румыния",
+            "Сальвадор",
+            "Самоа",
+            "Саудовская Аравия",
+            "Северная Ирландия",
+            "Северная Македония",
+        ].shuffled()
+        var totalWins = 0{
+            didSet {
+                newRound()
+            }
+        }
+        var totalLosses = 0{
+            didSet {
+                newRound()
+            }
+        }
+        
+        //MARK: - Methods
+        func enableButtons(_ enable: Bool = true) {
+            for button in letterButtons{
+                button.isEnabled = enable
+            }
+        }
+        
+        
+        func newRound () {
+            enableButtons(false)
+            guard !listOfWords.isEmpty else {
+                updateUI()
+                return
+            }
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+            updateUI()
+            enableButtons()
+        }
+        
+        func updateState(){
+            if currentGame.incorrectMovesRemaining < 1{
+                totalLosses += 1
+            }else if currentGame.guessedWord == currentGame.word{
+                totalWins += 1
+            }else{
+                updateUI()
+            }
+        }
+        
+        
+        func updateCorrectWordLabel(){
+            var displayWord = [String]()
+            for letter in currentGame.guessedWord{
+                displayWord.append(String(letter))
+            }
+            correctWordLabel.text = displayWord.joined(separator: " ")
+        }
+        
+        func updateUI(){
+            let movesRemaining = currentGame.incorrectMovesRemaining
+            let imageNumber = movesRemaining < 0 ? 0 : movesRemaining < 8 ? movesRemaining : 7
+            //let imageNumber = (movesRemaining + 64) % 8 другой вариант
+            let image = "Tree\(imageNumber)"
+            treeImageView.image = UIImage(named: image)
+            updateCorrectWordLabel()
+            scoreLabel.text = "Выигрыши:\(totalWins), Проигрыши:\(totalLosses)"
+        }
+    
+    //MARK: - UI Methods
+    @objc func  letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letter = sender.title(for: .normal)!
+        currentGame.playerGuessed(letter: Character(letter))
+        updateState()
     }
     
     func initLetterButtons (fontSize: CGFloat = 17 ) {
@@ -29,11 +203,12 @@ class ViewController: UIViewController {
             let title: String = buttonTitle == "_" ? "" : String(buttonTitle)
             let button = UIButton()
             if buttonTitle != "_" {
-            button.addTarget(self, action: #selector (buttonPressed(_ :)), for: .touchUpInside)
+            button.addTarget(self, action: #selector (letterButtonPressed(_ :)), for: .touchUpInside)
             }
             button.setTitle(title, for: [])
             button.setTitleColor(.systemBlue, for: .normal)
             button.setTitleColor(.systemTeal, for: .highlighted)
+            button.setTitleColor(.systemGray, for: .disabled)
             button.titleLabel?.textAlignment = .center
             button.titleLabel?.font = UIFont.systemFont(ofSize: fontSize)
             letterButtons.append(button)
@@ -105,6 +280,8 @@ class ViewController: UIViewController {
         view.addSubview(topStackView)
         
         updateUI(to: view.bounds.size)
+        
+        newRound()
 
     }
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
---
+++ apple pie code/game.swift
@@ -0,0 +1,39 @@
+//
+//  game.swift
+//  apple pie code
+//
+//  Created by Сергей Земсков on 04.08.2021.
+//
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    fileprivate var guessedLetters: [Character] = []
+    
+    init(word: String, incorrectMovesRemaining: Int) {
+        self.word = word
+        self.incorrectMovesRemaining = incorrectMovesRemaining
+    }
+    
+    var guessedWord: String {
+        var wordToShow = ""
+        for letter in word{
+            if guessedLetters.contains(Character(letter.lowercased())) || letter == "-" || letter == " " {
+                wordToShow += String(letter)
+            }else{
+                wordToShow += "_"
+            }
+            
+        }
+        return wordToShow
+    }
+    
+    
+    mutating func playerGuessed(letter: Character) {
+        let lowercasedLetter = Character(letter.lowercased())
+        guessedLetters.append(lowercasedLetter)
+        if !word.lowercased().contains(lowercasedLetter){
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
